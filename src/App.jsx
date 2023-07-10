3df2e77216f26844d033cbcf1ad4dda355fc01a2
Fabian Vargas <fabidev18@gmail.com>
2023-07-09 20:24:16 -0600

    Reestructuración - Se eliminó del componente "Principal" - El contenido de "Principal" se añadió a "App"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
 import './App.css'
-import Principal from './Components/Principal/Principal'
+import Sidebar from "./Components/Sidebar/Sidebar"
+import Paginas from "./Components/Páginas/Páginas"
+import Navegacion from "./Components/Nav/Navegacion"
 
 function App() {
+  const [pageState, setPageState] = useState({
+    home: "active",
+    user: "inactive",
+    curriculum: "inactive",
+    xp: "inactive"
+  });
+
+  const toggleState = (pageId) => {
+    setPageState((prevPageState) => {
+      const newPageState = {};
+
+      Object.keys(prevPageState).forEach((key) => {
+        newPageState[key] = key === pageId ? "active" : "inactive";
+      });
+
+      return newPageState;
+    });
+  };
+  
   return (
     <>
-    <Principal />
+      <div className="principal">
+        <Sidebar />
+        <Paginas pageState={pageState} />
+      </div>
+      <Navegacion click={toggleState} />
     </>
   )
 }

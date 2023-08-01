ef6d9b7b680af26f59a5d7151490149090de06d4
Fabian Vargas <fabidev18@gmail.com>
2023-08-01 17:19:27 -0600

    New: Formulario para enviar email

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,3 +1,4 @@
+import "./Contact.css"
 import { useFirstActivation } from "../../hooks/useFirstActivation"
 
 const Contact = ({state}) => {
@@ -7,7 +8,21 @@ const Contact = ({state}) => {
     <div className={`page_contacto ${state}`} id="contact">
       <h2 className="title">Contacto</h2>
       <div className="content">
-        Hola
+        <form action="">
+          <div className="input_container">
+            <label htmlFor="email">Correo</label>
+            <input type="email" className="form_input" id="email" placeholder="tucorreo@email.com" />
+          </div>
+          <div className="input_container">
+            <label htmlFor="asunto">Asunto</label>
+            <input type="text" className="form_input" id="asunto" placeholder="Asunto" />
+          </div>
+          <div className="input_container">
+            <label htmlFor="mensaje">Mensaje</label>
+            <textarea type="" className="form_input" id="mensaje" placeholder="Mensaje" />
+          </div>
+          <button type="button" className="form_button">Enviar Email</button>
+        </form>
       </div>
     </div>
   )

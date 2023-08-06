0f01faa706c13da1dd8eab8e4f8d82c55ba76044
Fabian Vargas <fabidev18@gmail.com>
2023-08-06 12:37:33 -0600

    New: Previsualización del email en el Formulario
    - Se creó el componente PrevisualizaciónCorreo para mostrar en tiempo real la estructura del correo

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,22 +1,16 @@
 import "./Contact.css"
-import { useRef, useState } from "react"
 import { useFirstActivation } from "../../hooks/useFirstActivation"
 import FormularioEmail from "../Formulario/FormularioEmail"
-import LoadSpin from "../Load-Spin/LoadSpin"
+import PrevisualizaciónCorreo from "../Prev-Correo/PrevisualizaciónCorreo"
 
 const Contact = ({state}) => {
-  const [isSending, setIsSending] = useState(false)
   const {pageContentRef} = useFirstActivation(state, "show")
-
-  const form = useRef()
-  
 
   return (
     <div className={`page_contacto ${state}`} id="contact" >
       <h2 className="title">Contacto</h2>
       <div className="content" ref={pageContentRef}>
-        <FormularioEmail reference={form} sendState={setIsSending} />
-        <LoadSpin state={isSending ? "active" : "inactive"}/>
+        <FormularioEmail />
       </div>
     </div>
   )
--- src/Components/Formulario/FormularioEmail.jsx
+++ src/Components/Formulario/FormularioEmail.jsx
@@ -1,18 +1,23 @@
 import "./FormularioEmail.css"
+import { useRef, useState } from "react"
 import { useForm } from 'react-hook-form'
 import emailjs from "@emailjs/browser"
+import LoadSpin from "../Load-Spin/LoadSpin"
 import InputContainer from '../InputContainer/InputContainer'
+import PrevisualizaciónCorreo from "../Prev-Correo/PrevisualizaciónCorreo"
 
-const FormularioEmail = ({ reference, sendState: setIsSending }) => {
-  const { register, handleSubmit, formState: { errors }, reset } = useForm({ mode: 'onChange' });
+const FormularioEmail = () => {
+  const [isSending, setIsSending] = useState(false)
+  const form = useRef()
   const inputs = ['nombre', 'email', 'asunto', 'mensaje'];
+  const { register, handleSubmit, formState: { errors }, reset, watch } = useForm({ mode: 'onChange' });
 
   const onSubmit = async (data) => {
     try {
       setIsSending(true);
-      await emailjs.sendForm('service_ajzh9ce', 'template_xyvun5k', reference.current, 'sIXy50I3C07pm9WK8');
+      await emailjs.sendForm('service_ajzh9ce', 'template_xyvun5k', form.current, 'sIXy50I3C07pm9WK8');
       setIsSending(false);
-      reference.current.reset();
+      reset()
     } catch (err) {
       alert('No se pudo enviar el email. Por favor, vuelve a intentarlo más tarde');
       console.error(err);
@@ -20,25 +25,29 @@ const FormularioEmail = ({ reference, sendState: setIsSending }) => {
   };
 
   return (
-    <form ref={reference} onSubmit={handleSubmit(onSubmit)}>
-      {inputs.map((input, idx) => (
-        <InputContainer
-          key={idx}
-          nameTag={input !== 'mensaje' ? 'input' : 'textarea'}
-          type={input !== 'email' ? 'text' : 'email'}
-          registerName={input}
-          labelText={input}
-          registerData={register(input, {
-            required: true,
-            pattern: input !== 'email' ? null : /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/,
-          })}
-          errors={errors}
-        />
-      ))}
-      <button type="submit" className="form_button">
-        Enviar Email
-      </button>
-    </form>
+    <div className="form_grid">
+      <form ref={form} onSubmit={handleSubmit(onSubmit)}>
+        {inputs.map((input, idx) => (
+          <InputContainer
+            key={idx}
+            nameTag={input !== 'mensaje' ? 'input' : 'textarea'}
+            type={input !== 'email' ? 'text' : 'email'}
+            registerName={input}
+            labelText={input}
+            registerData={register(input, {
+              required: true,
+              pattern: input !== 'email' ? null : /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/,
+            })}
+            errors={errors}
+          />
+        ))}
+        <button type="submit" className="form_button">
+          Enviar Email
+        </button>
+      <LoadSpin state={isSending ? "active" : "inactive"}/>
+      </form>
+      <PrevisualizaciónCorreo valores={watch()}/>
+    </div>
   );
 };
 

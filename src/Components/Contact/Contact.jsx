c2762269715dfb57910591902c9fc855505709a4
Fabian Vargas <fabidev18@gmail.com>
2023-08-06 21:20:00 -0600

    Configuración de eslintrc
    Se formateó todos los archivos con eslint

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,18 +1,18 @@
-import "./Contact.css"
-import { useFirstActivation } from "../../hooks/useFirstActivation"
-import FormularioEmail from "../Formulario/FormularioEmail"
+import "./Contact.css";
+import { useFirstActivation } from "../../hooks/useFirstActivation";
+import FormularioEmail from "../Formulario/FormularioEmail";
 
-const Contact = ({state}) => {
-  const {pageContentRef} = useFirstActivation(state, "show")
+const Contact = ({ state }) => {
+  const { pageContentRef } = useFirstActivation(state, "show");
 
   return (
-    <div className={`page_contacto ${state}`} id="contact" >
+    <div className={`page_contacto ${state}`} id="contact">
       <h2 className="title">Contacto</h2>
       <div className="content" ref={pageContentRef}>
         <FormularioEmail />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;

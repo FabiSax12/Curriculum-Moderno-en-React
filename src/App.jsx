fbc2ce02918c522c5a61ac80540dbe388a211e07
Fabian Vargas <fabidev18@gmail.com>
2023-08-26 11:25:23 -0600

    fix: BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import Sidebar from "./Components/Sidebar";
 import Pages from "./Components/Pages";
 import Navigation from "./Components/Navigation";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import AboutMe from "./Components/AboutMe";
+import Resume from "./Components/Resume";
+import Proyects from "./Components/Proyects";
+import Contact from "./Components/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <section className="principal">
         <Sidebar />
         <Pages />
       </section>
       <Navigation />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
